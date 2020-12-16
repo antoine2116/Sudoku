047dab91ee1a3467b0930b8675e0b1384ba6ae75
antoine2116 <51358897+antoine2116@users.noreply.github.com>
2020-12-16 15:08:39 +0100

    Initialisation

---
+++ grille.py
@@ -0,0 +1,96 @@
+import json
+import os.path
+import math
+
+class Grille:
+    # Constructeur
+    def __init__(self, n):
+        self.n = n
+
+        path_joueur = 'grilles/joueurs/grille_' + str(n) + '.json'
+        path_solution = 'grilles/solutions/grille_' + str(n) + '.json'
+
+        # Si on a un fichier jouer, on l'ouvre
+        if os.path.isfile(path_joueur):
+            fichier = open(path_joueur, 'r')
+
+        # Sinon, on ouvre le fichier solution (fichier vide)
+        elif os.path.isfile(path_solution):
+            fichier = open(path_solution, 'r')
+
+        self.m = json.load(fichier)
+        fichier.close()
+
+    #Permet d'afficher la grille
+    def afficher(self):
+        for l in range(self.n):
+            ligne = ""
+            if l == 3 or l == 6: # 3 et 6 temporaire (fonctionne uniquement sur des grille 9x9)
+                print("---------------------")
+            for c in range(self.n):
+                if c == 3 or c == 6:  # 3 et 6 temporaire (fonctionne uniquement sur des grille 9x9)
+                    ligne += "| "
+                if self.m[l][c] > 0:
+                    ligne += str(self.m[l][c]) + " "
+                else:
+                    ligne += "0 "
+            print(ligne)
+
+    # Permet de savoir si la grille est resolue
+    def resolue(self):
+        for l in range(self.n):
+            for c in range(self.n):
+                if (self.m[l][c] < 0):
+                    return False
+
+    # Permet d'afficher le chiffre trouve
+    def decouvrir(self, l, c):
+        self.m[l][c] = -self.m[l][c]
+
+    # Permet de proposer un nombre
+    def proposer(self, p, l, c):
+        approved = True
+        # On ajuste les index (saisie plus amicale)
+        l = l - 1
+        c = c - 1
+
+        # Ligne
+        for col in range(self.n):
+            if self.m[l][col] == p:
+                approved = False
+
+        # Colonne
+        for lig in range(self.n):
+            if self.m[lig][c] == p:
+                approved = False
+
+        # Block
+        diviseur = int(self.n/3)
+        c_block = int(math.floor(c/diviseur) * diviseur)
+        l_block = int(math.floor(l/diviseur) * diviseur)
+
+        for i in range(self.n//3):
+            for y in range(self.n//3):
+                if self.m[l_block + i][c_block + y] == p:
+                    approved = False
+        
+        if approved:
+            self.decouvrir(l, c)
+
+        return approved    
+
+    def sauvegarder(self):
+        path_joueur = 'grilles/joueurs/grille_' + str(self.n) + '.json'
+        # Si on a un fichier joueur, on l'ouvre
+        if os.path.isfile(path_joueur):
+            fichier = open(path_joueur, 'w');
+        # Sinon, on en cree un
+        else:
+            fichier = open(path_joueur, 'a')
+        
+        # On remplace / ajoute la nouvelle grille
+        json.dump(self.m, fichier, indent=4, sort_keys=True)
+        fichier.close()
+
+    
+    
---
+++ main.py
@@ -0,0 +1,21 @@
+from grille import *
+
+grille = Grille(9)
+grille.afficher()
+
+# Test reussite
+if grille.proposer(6, 1, 4):
+    print("Bravo ! \n")
+else:
+    print("Echec... \n")
+
+grille.afficher()
+
+# Test Echec
+if grille.proposer(9, 2, 1):
+    print("Bravo ! \n")
+else:
+    print("Echec... \n")
+
+grille.afficher()
+grille.sauvegarder()
