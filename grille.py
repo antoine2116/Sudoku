33a0386e8c2acb2d069caf4ce80c80bcebdd025f
Louis <dussoulier.louis@hotmai.fr>
2021-01-20 21:17:25 +0100

    Ajout : Brute force

--- grille.py
+++ grille.py
@@ -1,15 +1,19 @@
 import json
 import os.path
 import math
+import time
 
 class Grille:
     # Constructeur
     def __init__(self, n):
         self.n = n
+        self.tabL =[]
+        self.tabC = []
+        self.index = -1
 
-        path_joueur = 'grilles/joueurs/grille_' + str(n) + '.json'
+        path_joueur = 'grilles/joueurs/grille_' + str(n) + '0.json'
         path_indices = 'grilles/joueurs/grilleIndices' + str(n) + '.json'
-        path_solution = 'grilles/solutions/grille_' + str(n) + '.json'
+        path_solution = 'grilles/solutions/grille_' + str(n) + '0.json'
 
         # Si on a un fichier joueur, on l'ouvre
         if os.path.isfile(path_joueur):
@@ -37,30 +41,30 @@ class Grille:
                     if c == 3 or c == 6:  # 3 et 6 temporaire (fonctionne uniquement sur des grille 9x9)
                         ligne += "| "
                     if self.m[l][c] > 0:
-                        if self.m[l][c] > 9:
-                            ligne += self.convertisseur(self.m[l][c]) + " "
-                        else:
+                        #if self.m[l][c] > 9:
+                            #ligne += self.convertisseur(self.m[l][c]) + " "
+                        #else:
                             ligne += str(self.m[l][c]) + " "
                     else:
                         ligne += "0 "
                 print(ligne)
 
-        if self.n == 16:
-            for l in range(self.n):
-                ligne = ""
-                if l == 4 or l == 8 or l == 12:
-                    print("-------------------------------------")
-                for c in range(self.n):
-                    if c == 4 or c == 8 or c == 12:
-                        ligne += "| "
-                    if self.m[l][c] > 0:
-                        if self.m[l][c] > 9:
-                            ligne += self.convertisseur(self.m[l][c]) + " "
-                        else:
-                            ligne += str(self.m[l][c]) + " "
-                    else:
-                        ligne += "0 "
-                print(ligne)
+        #if self.n == 16:
+            #for l in range(self.n):
+                #ligne = ""
+                #if l == 4 or l == 8 or l == 12:
+                    #print("-------------------------------------")
+                #for c in range(self.n):
+                    #if c == 4 or c == 8 or c == 12:
+                        #ligne += "| "
+                    #if self.m[l][c] > 0:
+                        #if self.m[l][c] > 9:
+                            #ligne += self.convertisseur(self.m[l][c]) + " "
+                #else:
+                            #ligne += str(self.m[l][c]) + " "
+                #else:
+                        #ligne += "0 "
+                #print(ligne)
 
     #Permet de passer les nombres en lettre
     def convertisseur(self, chiffre):
@@ -75,8 +79,8 @@ class Grille:
                     return False
 
     # Permet d'afficher le chiffre trouve
-    def decouvrir(self, l, c):
-        self.m[l][c] = -self.m[l][c]
+    def decouvrir(self,p, l, c):
+        self.m[l][c] = p
 
     # Permet de proposer un nombre
     def proposer(self, p, l, c):
@@ -106,7 +110,7 @@ class Grille:
                     approved = False
         
         if approved:
-            self.decouvrir(l, c)
+            self.decouvrir(p,l, c)
 
         return approved    
 
@@ -136,7 +140,7 @@ class Grille:
         n = self.n
         tab = []
 
-        for i in  range(n):
+        for i in range(n):
             i = i +1
             if self.proposer(i,l,c):
                 tab.append(i)
@@ -145,3 +149,99 @@ class Grille:
             print (tab[y])
 
 
+    def caseEmpty(self,l,c):
+        if self.m[l][c] <= 0:
+            return 0
+        else:
+            return 1
+
+
+    def retour(self,l,c):
+        return self.m[l][c]
+
+
+    def forceBack(self,p,l,c): #regarder le probleme
+        print(p)
+        check = 0
+        i = 1
+        y = 2
+
+        while check == 0:
+            self.forceZero(l, c)
+            print("p = "+str(p) + "i = "+str(i))
+            if p+i > 9:
+                print("supérieur à 9")
+                self.back(-y)
+                print ("-y "+str(-y))
+                y = y +1
+                i = 1
+            if self.proposer(p+i,l+1,c+1) == True:
+                print("force l " + str(l + 1) + " force c " + str(c + 1) + "  t=" + str(p+i))
+                self.afficher()
+                del self.tabL[y+1:]
+                check = 1
+            else:
+                i = i + 1
+
+
+
+
+    def stockTentatives(self,l,c):
+        self.tabL.append(l)
+        self.tabC.append(c)
+
+
+    def back(self,index):
+
+        l = self.tabL[index]
+        c = self.tabC[index]
+        t = self.retour(l,c)
+        #print(index)
+        print("ancien l "+str(l)+" ancien c "+str(c)+" t "+str(t))
+
+        self.forceBack(t,l,c)
+
+    def forceZero(self,l,c):
+        self.m[l][c] = 0
+
+
+
+##### brute force #####
+    # Test la grille ligne par ligne
+
+    def testValeur(self,grid:list,i:int,j:int) -> list:
+
+      LigneRempli = [0,0,0,0,0,0,0,0,0,0]
+      for IndexLigne in range(9):
+        LigneRempli[grid[i][IndexLigne]]=1
+      for IndexColonne in range(9):
+          LigneRempli[grid[IndexColonne][j]]=1
+      # Pour un carré de 3 X 3
+      colonneStart,ligneStart = i//3*3,j//3*3
+      for IndexLigne in range(ligneStart,ligneStart + 3):
+        for IndexColonne in range(colonneStart,colonneStart + 3):
+          LigneRempli[grid[IndexColonne][IndexLigne]]=1
+      return(LigneRempli)
+
+    def AlgoBruteForce(self):
+        count = 1
+        countTours = 0
+        while count != 0:
+            count2 = 0
+            for i in range(9):
+                for j in range(9):
+                    countTours = countTours + 1
+                    if self.m[i][j] == 0:
+                        intList = self.testValeur(self.m, i, j)
+                        count2 += intList[0]
+                        if sum(intList[1:]) == 8:
+                            self.m[i][j] = intList[1:].index(0) + 1
+            count = count2
+        print("count "+str(countTours))
+        return (self.m)
+
+
+    def lancerBruteForce(self):
+        self.grilleRemplie = self.AlgoBruteForce()
+        for i in range(9):
+          print(self.grilleRemplie[i])
